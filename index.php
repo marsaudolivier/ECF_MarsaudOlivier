1a7fa7e6727a721a423afc0b474b2f576049ad82
Marsaud olivier <cougies79700@gmail.com>
2023-08-11 20:07:25 +0200

    Templates: Ajout Headers + CSS + media query

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once("./templates/header.php");
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>test</h1>
+            <h1>teste</h1>
         </div>
     </div>
 </div
