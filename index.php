6f19d539a038dabdd0f0ae4b312a5a554b09cf83
Marsaud olivier <cougies79700@gmail.com>
2023-08-11 13:47:05 +0200

    templates: Création page header, footer et index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+ require_once("./templates/header.php");
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Test</h1>
+        </div>
+    </div>
+</div>
+<?php
+    require_once("./templates/footer.php");
+?>
