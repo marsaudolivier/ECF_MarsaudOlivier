a44d4129222e7f5642f222edf6d6df1ba49ee780
Marsaud olivier <cougies79700@gmail.com>
2023-08-11 22:29:59 +0200

    Templates: Ajout footer + CSS (en attente Synchronisation BDD)

--- templates/Footer.php
+++ templates/Footer.php
@@ -1,7 +1,53 @@
+<!--Ajout Pied de page-->
+<div class="d-flex footer_bandeau text-center text-uppercase p-3">
+    <div class="row col-12">
+        <div class="col-12 col-md-5">
+            <!-- Horaire du garage -->
+            <h3 class="text-success">Horaires d’ouverture</h3>
+            <p class="text-info">lun.: 08h45-12;00, 14h00-18:00</br>
+                mar.: 08h45-12;00, 14h00-18:00</br>
+                mer.: 08h45-12;00, 14h00-18:00</br>
+                jeu.: 08h45-12;00, 14h00-18:00</br>
+                ven.: 08h45-12;00, 14h00-18:00</br>
+                sam.: 08h45-12;00, 14h00-18:00</br>
+                dim.: fermé</p>
+        </div>
+        <!-- Ajout logo + social -->
+        <img class="col-0 col-md-2 logo_footer hidden" src="../assets/images/Logo Parrot.svg" alt="Logo Garage V. Parrot"></img>
+        <div class="col-12 col-md-5 p-3">
+            <h3 class="text-danger text-uppercase align-items-center text-center">Contacter Nous</h3>
+            <p class="text-info">Garage V. Parrot</br>
+                1 rue du garage</br>
+                Toulouse</br>
+                05 46 00 00 00</br>
+            <div class="hidden">
+                <a href="https://www.twitter.com/" title="Notre twitter">
+                    <img src="../assets/images/twitter.svg" height="35px" width="35px">
+                </a>
+                <a href="https://www.facebook.com/" title="Notre Facebook">
+                    <img src="../assets/images/facebook.svg" height="35px" width="35px" class=>
+                </a>
+                <a href="https://www.instagram.com/" title="Notre instagram">
+                    <img src="../assets/images/instagram.svg" height="35px" width="35px">
+                </a>
+            </div>
+        </div>
+        <div class="col-12 text-success text-center p-1">
+        <a class="navigation_link navbar-text p-5" href="">politique des cookies</a>
+        <a class="navigation_link navbar-text p-5" href="">Mentions Légales</a>
+        </div>
+        <!-- Signature bas de page -->
+        <div class="col-12 text-success text-center p-1">
+            ECF Mr Marsaud Olivier
+        </div>
+    </div>
+
+</div>
 <!--Ajout JS-->
 <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
+
 </html>
