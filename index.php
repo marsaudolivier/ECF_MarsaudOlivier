70fd6a1df05d5a7b2331b9a7e294e19deac6621d
Marsaud olivier <cougies79700@gmail.com>
2023-08-11 20:01:47 +0200

    Templates; Ajout Headers + CSS + media query

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
- require_once("./templates/header.php");
+require_once("./templates/header.php");
 ?>
-</head>
-<body>
-
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Test</h1>
+            <h1>test</h1>
         </div>
     </div>
-</div>
+</div
 <?php
-    require_once("./templates/footer.php");
+require_once("./templates/footer.php");
 ?>
--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,3 +7,24 @@
     <!-- CSS + Font Aref Rugaa -->
     <link rel="stylesheet" href="../styles/main.css">
     <link href="https://fonts.googleapis.com/css2?family=Aref+Ruqaa+Ink&family=Great+Vibes&display=swap" rel="stylesheet">
+</head>
+<body>
+  <!--Intégration de la navbar Responsive-->
+  <header class="navigation_bandeau d-flex">
+      <img src="../assets/images/Logo Parrot.svg" class="logo" alt="Logo de notre garage V parrot">
+    <h1 class="d-flex align-items-center text-center p-2">Garage
+      V. Parrot</h1>
+    <nav class="navbar navbar-expand-lg navbar-info text-info" data-bs-theme="dark"> 
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse text-primary navigation_link" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="navigation_link navbar-text" href="./Index.php">Acceuil</a>
+            <a class="navigation_link navbar-text" href="ventes.php">Ventes</a>
+            <a class="navigation_link navbar-text" href="">Admin</a>
+          </div>
+        </div>
+      </nav>   
+</header>
