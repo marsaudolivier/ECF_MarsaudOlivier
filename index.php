0bc2799a5b5ae7f9ef20ab4428f9fad83b4b6226
Marsaud olivier <cougies79700@gmail.com>
2023-08-12 22:18:28 +0200

    Fonctionnalité: Ajout titres page +CSS

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ require_once("./templates/header.php");
 ?>
  <!--Intégration de la Fil ariane-->
  <a href="index.html" class="text-success p-2">Acceuil</a>
+ <!--Ajout titre de la page-->
+<h2>Bienvenue au Garage 
+</br>  V. Parrot</h2>
 <?php
 require_once("./templates/footer.php");
 ?>
