9fd86c9ac996508b688b1b85e153f35b0aac7a82
Marsaud olivier <cougies79700@gmail.com>
2023-08-12 22:21:25 +0200

    Contenu: Ajout photo1 index

--- index.php
+++ index.php
@@ -6,6 +6,11 @@ require_once("./templates/header.php");
  <!--Ajout titre de la page-->
 <h2>Bienvenue au Garage 
 </br>  V. Parrot</h2>
+<!--Ajout photo + style-->
+<div class="p-4 d-flex">
+  <img src="./assets/images/index1.jpg" alt="Notre mécanicien en pleine mécanique" class="index_image">
+</div>
+
 <?php
 require_once("./templates/footer.php");
 ?>
