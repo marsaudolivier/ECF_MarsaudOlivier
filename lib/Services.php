422fb47c19ef61dbd32865df86a4871559022386
Marsaud olivier <cougies79700@gmail.com>
2023-08-13 23:28:46 +0200

    Css: Ajout style modal + services

--- lib/Services.php
+++ lib/Services.php
@@ -5,25 +5,27 @@ function Services($pdo)
     $query = $pdo->prepare($sql);
     $query->execute();
     $Services = $query->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($Services as $Service) {
 ?>
-        <ul class="service p-4 m-3">
+    <ul class="service p-4 m-3">
+        
+        <?php  foreach ($Services as $Service) {
+            ?>
             <div class="d-flex p-4">
                 <li><?= $Service['titre'] ?></li> <button type="button" class="btn btn-secondary index_bouton" data-bs-toggle="modal" data-bs-target="#<?= $Service['Id_Services'] ?>">
                     Plus d'info
                 </button>
             </div>
             </div>
-        </ul>
-        <?php 
+            <?php 
     } ?>
+</ul>
           <!-- Modal -->
 <?php foreach ($Services as $Service) {?>
   <div class="modal fade p-5" id="<?= $Service['Id_Services'] ?>" tabindex="-1" aria-labelledby="<?= $Service['Id_Services'] ?>" aria-hidden="true">
     <div class="modal-dialog">
-      <div class="modal-content">
+      <div class="modal-content test">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="<?= $Service['Id_Services'] ?>"><?= $Service['titre'] ?></h1>
+          <h2 class="modal-title fs-5" id="<?= $Service['Id_Services'] ?>"><?= $Service['titre'] ?></h2>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
