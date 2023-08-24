61bed915c8824d4e3dc95dbf62af72f3285a7c3e
Marsaud olivier <cougies79700@gmail.com>
2023-08-24 20:57:14 +0200

    libs:Ajout class Energies

--- lib/annonces.php
+++ lib/annonces.php
@@ -214,30 +214,3 @@ class Modeles extends Marques
         $this->SetId_Marques($Id_Marques);
     }
 }
-class Energies extends consommer
-{
-    protected string $energie;
-    protected int $Id_Energies;
-
-    protected function GetEnergie()
-    {
-        return $this->energie;
-    }
-    protected function GetId_Energies()
-    {
-        return $this->Id_Energies;
-    }
-    protected function SetEnergie($energie)
-    {
-        $this->energie = $energie;
-    }
-    protected function SetId_Energies($Id_Energies)
-    {
-        $this->Id_Energies = $Id_Energies;
-    }
-    public function __construct($energie, $Id_Energies)
-    {
-        $this->SetEnergie($energie);
-        $this->SetId_Energies($Id_Energies);
-    }
-}
