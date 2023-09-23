c498e8d1d5b49dc801839367a5a884812bffb74d
Marsaud olivier <cougies79700@gmail.com>
2023-09-23 22:36:58 +0200

    fonctionnalité: Ajout page Cookies

---
+++ cookies.php
@@ -0,0 +1,19 @@
+<?php
+require_once('./templates/header.php');
+?>
+<h2 class="p-5">Notification sur l'utilisation des cookies</h2>
+<section class="Mentions_legales p-5">
+<p class="Mentions_legales_titre"><strong>Utilisation:</strong></p>
+<p>Ce site utilise des cookies pour améliorer votre expérience en tant qu'employé. Ces cookies sont essentiels pour la gestion de vos droits 
+    et privilèges au sein de notre système.</p>
+    <p class="Mentions_legales_titre"><strong>Que sont les cookies ?</strong></p>
+<p>Un cookie est un petit fichier texte stocké sur votre ordinateur ou appareil mobile par le navigateur web que vous utilisez. Ces fichiers 
+    nous aident à vous identifier en tant qu'employé et à personnaliser votre expérience en fonction de vos préférences.</p>
+<p class="Mentions_legales_titre"><strong>Comment utilisons-nous les cookies ?</strong></p>
+<p>Les cookies que nous utilisons sont strictement nécessaires à votre navigation et à la gestion de vos droits en tant qu'employé. Ils ne 
+    collectent pas d'informations personnelles et ne sont pas utilisés à des fins de marketing ou de suivi.</p>
+<p class="Mentions_legales_titre"><strong>Votre consentement implicite</strong></p>
+<p>En continuant à utiliser ce site, vous consentez à l'utilisation de ces cookies essentiels pour la gestion de votre expérience en tant
+     qu'employé. Si vous souhaitez en savoir plus sur notre utilisation des cookies, veuillez consulter notre Politique des Cookies.</p>
+ <p>Merci de votre compréhension et de votre collaboration.  </p>    
+</section>
