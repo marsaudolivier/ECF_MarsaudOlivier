aea1d9dd1f10342a68655db00708fd7be27676d7
Marsaud olivier <cougies79700@gmail.com>
2023-09-23 22:07:01 +0200

    fix: Ponctuation

--- MentionsLegales.php
+++ MentionsLegales.php
@@ -17,27 +17,27 @@ include_once('./templates/header.php');
     </p>
     <p class="Mentions_legales_titre"><strong>Hébergeur du site:</strong></p>
     <p><strong>Nom de l'hébergeur :</strong> alwaysdata.com</br>
-    <p class="Mentions_legales_titre"><strong>Propriété intellectuelle</strong></p>
+    <p class="Mentions_legales_titre"><strong>Propriété intellectuelle:</strong></p>
     <p>Le contenu de ce site (textes, images, vidéos, etc.) est protégé par les droits de propriété intellectuelle.
         Toute reproduction ou utilisation sans autorisation préalable est interdite.</p>
-    <p class="Mentions_legales_titre"><strong>Données personnelles</strong></p>
+    <p class="Mentions_legales_titre"><strong>Données personnelles:</strong></p>
     <p>Conformément à la loi Informatique et Libertés du 6 janvier 1978, vous disposez d'un droit d'accès, de rectification et de
         suppression des données vous concernant. Pour exercer ce droit, veuillez nous contacter par e-mail à l'adresse suivante : marsaudolivier@gmail.com.</p>
-    <p class="Mentions_legales_titre"><strong>Cookies</strong></p>
+    <p class="Mentions_legales_titre"><strong>Cookies:</strong></p>
     <p>Ce site utilise des cookies pour améliorer l'expérience utilisateur. En naviguant sur ce site, vous acceptez l'utilisation des cookies.</p></br>
 
-    <h2>À propos de ce site</h2>
+    <h2>À propos de ce site:</h2>
     <p>Ce site a été créé à des fins d'évaluation dans le cadre de l'examen ECF (Épreuve de Connaissance et de Fin de formation) pour l'école Studi.</p>
-    <p class="Mentions_legales_titre"><strong>Objectif</strong></p>
+    <p class="Mentions_legales_titre"><strong>Objectif:</strong></p>
     <p>L'objectif principal de ce site est de démontrer les compétences acquises dans le domaine du développement web dans le cadre de la formation dispensée par l'école Studi. 
         Il vise à présenter un échantillon représentatif des connaissances et des compétences acquises tout au long de la formation.</p>
-    <p class="Mentions_legales_titre"><strong>Examen ECF</strong></p>
+    <p class="Mentions_legales_titre"><strong>Examen ECF:</strong></p>
     <p>L'examen ECF est une étape cruciale dans notre parcours de formation. Il évalue nos compétences, nos connaissances et notre aptitude à appliquer les concepts enseignés 
         dans des projets concrets.</p>
-        <p class="Mentions_legales_titre"><strong>Utilisation pédagogique</strong></p>
+        <p class="Mentions_legales_titre"><strong>Utilisation pédagogique:</strong></p>
         <p>Ce site a été conçu dans le but de mettre en pratique les enseignements reçus et d'exposer notre savoir-faire en matière de développement web. Il est destiné à être
              évalué dans le cadre de l'examen ECF de l'école Studi.</p>
-             <p class="Mentions_legales_titre"><strong>Confidentialité</strong></p>
+             <p class="Mentions_legales_titre"><strong>Confidentialité:</strong></p>
              <p>Toutes les données et les contenus présents sur ce site sont destinés à l'évaluation dans le cadre de l'examen ECF de l'école Studi. Toute utilisation, reproduction
                  ou diffusion en dehors de cette contexte est strictement interdite.</p>
 
