dcbeecfcacc96735f7b57b5db8d68e654094a3fb
Marsaud olivier <cougies79700@gmail.com>
2023-09-25 16:05:01 +0200

    Commentaires: Connexion ventes et index

--- Connexion.php
+++ Connexion.php
@@ -1,49 +1,50 @@
 <?php
 require_once("./templates/header.php");
 require_once("./lib/utilisateurs.php");
-if(!empty($_COOKIE)){
+//Vérificaion cookie de connexion
+if (!empty($_COOKIE)) {
     $mail = $_COOKIE['mail'];
     $token = $_COOKIE['token'];
     $user = utilisateurs::UtilisateurVerificationToken($pdo, $mail, $token);
     header('Location: ./admin.php');
-}else{
+} else {
 ?>
-<h2 class="p-3">Connexion administrateur</h2>
-<div class="admin_conteneur p-5 container align-middle ">
-    <h3>Veuillez entrer vos identifiants pour vous connecter :</h3>
-    <form enctype="multipart/form-data" method="POST">
-        <div class="col-6 mx-auto p-4 logTexte">
-           <label for="email ">Adresse e-mail :</label><br>
-        <input type="email" id="email" name="email" class="form-control border border-success" required >
-        </div>
-        <div class="col-6 mx-auto p-4 logTexte">
-              <label for="password ">Mot de passe :</label><br>
-        <input type="password" id="password" name="password"class="form-control border border-success" required><br><br>
-        </div>
-        <input type="submit" value="Connexion" name="test">
-    </form>
-</div>
-<div class="p-5"></div>
+    <h2 class="p-3">Connexion administrateur</h2>
+    <div class="admin_conteneur p-5 container align-middle ">
+        <h3>Veuillez entrer vos identifiants pour vous connecter :</h3>
+        <form enctype="multipart/form-data" method="POST">
+            <div class="col-6 mx-auto p-4 logTexte">
+                <label for="email ">Adresse e-mail :</label><br>
+                <input type="email" id="email" name="email" class="form-control border border-success" required>
+            </div>
+            <div class="col-6 mx-auto p-4 logTexte">
+                <label for="password ">Mot de passe :</label><br>
+                <input type="password" id="password" name="password" class="form-control border border-success" required><br><br>
+            </div>
+            <input type="submit" value="Connexion" name="test">
+        </form>
+    </div>
+    <div class="p-5"></div>
 
 <?php
-//Ajout sécurité faille XSS
-if (isset($_POST['test'])) { 
-$mail = htmlspecialchars($_POST['email']);
-$password = htmlspecialchars($_POST['password']);
-require_once('./lib/utilisateurs.php');
-require_once('./lib/pdo.php');
-$user = utilisateurs::loginUser($pdo, $mail, $password);
+    //Ajout sécurité faille XSS
+    if (isset($_POST['test'])) {
+        $mail = htmlspecialchars($_POST['email']);
+        $password = htmlspecialchars($_POST['password']);
+        require_once('./lib/utilisateurs.php');
+        require_once('./lib/pdo.php');
+        $user = utilisateurs::loginUser($pdo, $mail, $password);
 
-if ($user) {
-    $token =bin2hex(random_bytes(50));
-    utilisateurs::tokenAdd($pdo, $token, $password, $mail);
-    setcookie('token', $token, time() + 3600);
-    setcookie('mail', $mail, time() + 3600);
-    header("location: admin.php");
-    exit();
-} else {
-    echo "Identifiants invalides. Veuillez réessayer.";
-}
-}
+        if ($user) { // création d'un clef hexa sur 50 caractère token connexion
+            $token = bin2hex(random_bytes(50));
+            utilisateurs::tokenAdd($pdo, $token, $password, $mail);
+            setcookie('token', $token, time() + 3600);
+            setcookie('mail', $mail, time() + 3600);
+            header("location: admin.php");
+            exit();
+        } else {
+            echo "Identifiants invalides. Veuillez réessayer.";
+        }
+    }
 }
 require_once("./templates/Footer.php");
--- ventes.php
+++ ventes.php
@@ -4,8 +4,9 @@ require_once("./templates/slider.php");
 require_once("./lib/annonces.php");
 require_once("./templates/card_vehicule.php");
 require_once("./templates/contacts.php");
+//Vérification si le formulaire contact bien transmit avec un message pour utilisateur intéractif
 if (isset($_GET['success']) && $_GET['success'] == 1) {
-    echo '<script>alert("Formulaire transmis avec succès.");</script>';
+    echo '<script>alert("Formulaire transmis avec succès. Notre équipe reviendra vers vous prochainement");</script>';
 }
 ?>
 <!--Intégration de la Fil ariane-->
@@ -13,24 +14,16 @@ if (isset($_GET['success']) && $_GET['success'] == 1) {
 <a href="ventes.php" class="text-success p-2">/Ventes</a>
 <!--Ajout titre de la page-->
 <h2>Voiture d’occasion</h2>
-<div class="album py-5"id="annoncesDetailContainer">
-<?php
-// Intégration du slider
-Slider();
-?>
-
-        <div class="container" >
-            <div class="row row-cols-1  row-cols-md-2 row-cols-lg-3 g-3"id="annoncesContainer" >
+<div class="album py-5" id="annoncesDetailContainer">
+    <?php
+    // Intégration du slider
+    Slider();
+    ?>
+    <div class="container">
+        <div class="row row-cols-1  row-cols-md-2 row-cols-lg-3 g-3" id="annoncesContainer">
         </div>
     </div>
 </div>
 <?php
-
-
-
-
-
-
-
 require_once("./templates/Footer.php");
 ?>
