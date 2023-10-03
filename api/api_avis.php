4e96284314f1d55ece874df4811d389d582fb1e7
Marsaud olivier <cougies79700@gmail.com>
2023-10-03 12:40:47 +0200

    Fix: Oublie condition validation pour les avis

--- api/api_avis.php
+++ api/api_avis.php
@@ -4,7 +4,9 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = 'SELECT * FROM Avis INNER JOIN Validations v 
         on Avis.Id_Validations = v.Id_Validations 
-        ORDER BY Id_Avis DESC';
+        WHERE v.Id_Validations = "2"
+        ORDER BY Id_Avis DESC'
+        ;
     $query = $pdo->query($sql);
     $avisData = $query->fetchAll(PDO::FETCH_ASSOC);
     header('Content-Type: application/json');
