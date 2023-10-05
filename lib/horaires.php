6b18d64be3b112febe068deb0172ee4395192665
Marsaud olivier <cougies79700@gmail.com>
2023-10-05 11:53:03 +0200

    Fix: Bug formulaire horaire vérification sur tout les jours avec une boucle

--- lib/horaires.php
+++ lib/horaires.php
@@ -26,9 +26,9 @@ function HorairesAdmin($pdo)
         <p class="text-info lib_horaires"><?= $jour['jour'] ?> : <?= $jour['heure_matin'] ?>, <?= $jour['heure_soir'] ?></p>
         <form action="adminHoraires.php" method="post" onsubmit="return validateFormAdmin()">
             <input type="hidden" name="Id_Jours" value="<?= $jour['Id_Jours'] ?>">
-            <input type="text" id="jour" name="jour" value="<?= $jour['jour'] ?>">
-            <input type="text" id="heure_matin" name="heure_matin" value="<?= $jour['heure_matin'] ?>">
-            <input type="text" id="heure_soir" name="heure_soir" value="<?= $jour['heure_soir'] ?>">
+            <input type="text" id="jour<?= $jour['Id_Jours'] ?>" name="jour" value="<?= $jour['jour'] ?>">
+            <input type="text" id="heure_matin<?= $jour['Id_Jours'] ?>" name="heure_matin" value="<?= $jour['heure_matin'] ?>">
+            <input type="text" id="heure_soir<?= $jour['Id_Jours'] ?>" name="heure_soir" value="<?= $jour['heure_soir'] ?>">
             <button type="submit" name="updateHorairesButton" class="btn btn-success">Modifier</button>
         </form>
 <?php
--- scripts/FormAdminHoraires.js
+++ scripts/FormAdminHoraires.js
@@ -1,30 +1,32 @@
-//fonction de controle formulaire employé
-function validateFormAdmin() {
-  //je récupère mes différent input par ID
-  let jour = document.getElementById("jour").value;
-  let heure_matin = document.getElementById("heure_matin").value;
-  let heure_soir = document.getElementById("heure_soir").value;
 
+//fonction de controle formulaire Horaires
+function validateFormAdmin() {
   //  accepter uniquement les jours de la semaine en français
   let joursSemaineRegEx =
-    /^(lundi|mardi|mercredi|jeudi|vendredi|samedi|dimanche)$/i;
-
+  /^(lundi|mardi|mercredi|jeudi|vendredi|samedi|dimanche)$/i;
+ // création d'une boucle pour récupéré les id de 1 a 7
+ for (let i = 1; i <= 7; i++) {
+  const jour = document.getElementById("jour" + i).value;
+  const heure_matin = document.getElementById("heure_matin"+ i).value;
+  const heure_soir = document.getElementById("heure_soir"+ i).value;
   if (!joursSemaineRegEx.test(jour)) {
-    alert(
-      "Le jour doit être lundi, mardi, mercredi, jeudi, vendredi, samedi ou dimanche."
-    );
-    return false;
+     alert(
+       `Le jour ${i} doit être lundi, mardi, mercredi, jeudi, vendredi, samedi ou dimanche.`
+     );
+     return false;
   }
-
-  // Regex pour le format des heures : 00:00,fermé ou vacances
+   // Regex pour le format des heures : 00:00,fermé ou vacances
   let heureRegEx =
     /^(0?[0-9]|1[0-9]|2[0-3])h(:?([0-5][0-9]))?-(0?[0-9]|1[0-9]|2[0-3])h(:?([0-5][0-9]))?$|^fermé$|^vacances$/;
 
   if (!heureRegEx.test(heure_matin) || !heureRegEx.test(heure_soir)) {
     alert(
-      "Les heures doivent être au format xxhxx-xxhxx,'fermé' ou 'vacances'."
+      `Les heures du jour ${i} doivent être au format xxhxx-xxhxx,'fermé' ou 'vacances'.`
     );
     return false;
   }
+ }
+
+ 
   return true;
 }
